35a84cdac05ddf05c30bbbea2ab8c524b4d439f3
uralm1 <uralm1@gmail.com>
2021-04-26 15:11:55 +0500

    Block checking after traffic statistics update

--- head/lib/Head/Controller/Stat.pm
+++ head/lib/Head/Controller/Stat.pm
@@ -13,15 +13,13 @@ sub trafstat {
   my $fmt = $self->req->headers->content_type // '';
   if ($fmt =~ m#^application/json$#i) {
     my $j = $self->req->json;
-    return $self->render(text=>'Bad json format', status=>503) unless $j;
-
-    $self->render_later;
+    return $self->render(text=>'Bad json format', status=>503) unless $j and ref($j) eq 'HASH';
 
     #$self->log->debug($self->dumper($j));
 
     if ($self->check_workers) {
       # statistics update is high priority task
-      $self->minion->enqueue(traf_stat => [time, $profs, $j] => {priority => 5});
+      $self->minion->enqueue(traf_stat => [time, $profs, $j] => {priority => 5, attempts => 5});
     } else {
       my $m = 'Statistics not accepted. Execution subsystem error.';
       $self->log->error($m);
@@ -29,73 +27,11 @@ sub trafstat {
       return $self->render(text=>"Execution subsystem not available", status=>503);
     }
 
-    # old mechanics
-    # update database in single transaction
-    my $db = $self->mysql_inet->db;
-    my $tx = eval { $db->begin };
-    unless ($tx) {
-      $self->log->error("Database begin transaction failure: $@");
-      return $self->render(text=>"Database begin transaction failure", status=>503);
-    }
-
-    $self->_submit_traf_stats($db, $tx, $profs, $j);
-    # now exit, execution continue asyncroniously
+    # successful enqueue
+    return $self->render(text=>"DONE ".scalar keys %$j, status=>200);
 
   } else {
     return $self->render(text=>'Unsupported content', status=>503);
-  }
-}
-
-
-# internal, starts recursive asyncronious submit process
-sub _submit_traf_stats {
-  my ($self, $db, $tx, $profs, $j, $s) = @_;
-  croak 'Bad arguments' unless ($db && $tx && $profs && $j);
-  $s //= [0, 0]; # reset counters on first run
-
-  my ($id, $v);
-  unless (($id, $v) = each %$j) {
-    eval { $tx->commit };
-    if ($@) {
-      $self->log->error("Database update transaction commit failure $@");
-      return $self->render(text=>"Database update transaction failure", status=>503);
-    }
-    # finished
-    my $m = "UPDATE FINISHED, submitted: $s->[0], updated: $s->[1]";
-    $self->log->debug($m);
-    #$self->dblog->info($m);
-    return $self->render(text=>"DONE $s->[1]/$s->[0]", status=>200); # SUCCESS
-  }
-
-  my $inb = $v->{in};
-  my $outb = $v->{out};
-  $s->[0]++; # count submitted
-  if ($inb > 0 or $outb > 0) {
-    my $rule = '';
-    for (@$profs) {
-      if ($rule eq '') { # first
-        $rule = 'profile IN ('.$db->quote($_);
-      } else { # second etc
-        $rule .= ','.$db->quote($_);
-      }
-    }
-    $rule .= ') AND' if $rule ne '';
-    #$self->log->debug("WHERE rule: *$rule*");
-
-    $db->query_p("UPDATE clients SET sum_in = sum_in + ?, sum_out = sum_out + ?, \
-sum_limit_in = IF(qs != 0, IF(sum_limit_in > ?, sum_limit_in - ?, 0), sum_limit_in) \
-WHERE $rule id = ?", $inb, $outb, $inb, $inb, $id)->then(sub {
-      my $results = shift;
-      $s->[1] += $results->affected_rows; # count updated
-    })->then(sub {
-      $self->_submit_traf_stats($db, $tx, $profs, $j, $s);
-    })->catch(sub {
-      my $err = shift;
-      $self->log->error("Database update failure id $id: $err");
-      return $self->render(text=>"Database update failure", status=>503);
-    });
-  } else {
-    $self->_submit_traf_stats($db, $tx, $profs, $j, $s);
   }
 }
 
--- head/lib/Head/Task/TrafStat.pm
+++ head/lib/Head/Task/TrafStat.pm
@@ -2,16 +2,103 @@ package Head::Task::TrafStat;
 use Mojo::Base 'Mojolicious::Plugin';
 
 use Mojo::mysql;
+use POSIX qw(strftime);
 
 sub register {
   my ($self, $app) = @_;
   $app->minion->add_task(traf_stat => sub {
     my ($job, $timestamp, $profs, $j) = @_;
-    die 'Bad job parameters' unless defined $timestamp && $profs && $j;
+    die 'Bad job parameters' unless defined $timestamp && $profs && $j && ref($profs) eq 'ARRAY' && ref($j) eq 'HASH';
     my $app = $job->app;
 
-    my $m = 'Traffic statistics processing /experimental/ job at '.localtime $timestamp;
+    my $m = 'Traffic statistics update job timestamp '.strftime "%H:%M:%S %d.%m", localtime($timestamp);
+    $app->log->info($m);
     $app->dblog->info($m, sync=>1);
+
+    # update database in single transaction
+    my $db = $app->mysql_inet->db;
+    my $tx = eval { $db->begin };
+    unless ($tx) {
+      $m = "Database begin transaction failure: $@";
+      $app->log->error($m);
+      $app->dblog->error($m, sync=>1);
+      return $job->fail;
+    }
+
+    my $submitted = 0; # counters
+    my $updated = 0;
+
+    my $rule = ''; # 'profile IN (plk, p2, p2) AND' or ''
+    for (@$profs) {
+      if ($rule eq '') { # first
+        $rule = 'profile IN ('.$db->quote($_);
+      } else { # second etc
+        $rule .= ','.$db->quote($_);
+      }
+    }
+    $rule .= ') AND' if $rule ne '';
+    #$app->log->debug("WHERE rule: *$rule*");
+
+    while (my ($id, $v) = each %$j) {
+      my $inb = $v->{in};
+      my $outb = $v->{out};
+      $submitted++;
+      if ($inb > 0 || $outb > 0) {
+        my $results = eval { $db->query("UPDATE clients SET sum_in = sum_in + ?, sum_out = sum_out + ?, \
+sum_limit_in = IF(qs != 0, IF(sum_limit_in > ?, sum_limit_in - ?, 0), sum_limit_in) \
+WHERE $rule id = ?", $inb, $outb, $inb, $inb, $id) };
+        if ($results) {
+          $updated += $results->affected_rows;
+        } else {
+          $m = "Database update failure id $id: $@";
+          $app->log->error($m);
+          $app->dblog->error($m, sync=>1);
+          return $job->fail;
+        }
+      }
+    } # loop by submitted clients
+
+    eval { $tx->commit };
+    if ($@) {
+      $m = "Database update transaction commit failure: $@";
+      $app->log->error($m);
+      $app->dblog->error($m, sync=>1);
+      return $job->fail;
+    }
+
+    # finished
+    $m = "UPDATE FINISHED, submitted: $submitted, updated: $updated";
+    $app->log->info($m);
+    $app->dblog->info($m, sync=>1);
+
+    # run block check after update
+    for my $jid (keys %$j) {
+      my $block_results = eval { $db->query("SELECT id, qs, email_notify, notified, profile FROM clients \
+WHERE $rule id = ? AND blocked = 0 AND sum_limit_in <= 0 AND qs > 0", $jid) };
+      unless ($block_results) {
+        $app->log->error("Block: database operation error: $@");
+      } else {
+        if (my $n = $block_results->hash) {
+          my $id = $n->{id};
+          my $qs = $n->{qs};
+          if ($qs == 1) {
+            # warn(1) client
+            if ($n->{email_notify}) {
+              $app->log->debug("Client to notify: $id, qs: $qs, $n->{profile}");
+              $app->minion->enqueue(notify_client => [$id]) unless $n->{notified};
+            }
+
+          } elsif ($qs == 2 || $qs == 3) {
+            # limit(2) or block(3) client
+            $app->log->debug("Client to block: $id, qs: $qs, $n->{profile}");
+            $app->minion->enqueue(block_client => [$id, $qs, $n->{profile}]);
+
+          } else {
+            $app->log->error("Unsupported qs $qs for client id $id.");
+          }
+        } # has one result
+      } # select without errors
+    } # loop by submitted clients
 
     $job->finish;
   });
